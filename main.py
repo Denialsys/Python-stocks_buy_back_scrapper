fd215c0780491145db2821e62be45b86a80a12a6
Jenver <denialsys@gmail.com>
2022-12-06 01:40:58 +0800

    Added the definition for gathering the company news

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import requests
 
-
+NEWS_RESOURCE_BASE_URL = 'https://firstmetro.cdn.technistock.net/news/n-psendc-hist-json.asp?code={0}'
 target_company_code = 'MEG'
-NEWS_RESOURCE_URL = 'https://firstmetro.cdn.technistock.net/news/n-psendc-hist-json.asp?code='
+NEWS_RESOURCE_URL = NEWS_RESOURCE_BASE_URL.format(target_company_code)
+request_timeout = 5 ## Server might take some time to respond so use 5 seconds
 
-def gather_reports(report_url):
+
+def get_company_news(url):
+    '''
+
+    :param url: URL for the company news
+    :return:
+    '''
+    result = ''
+    try:
+
+        result = requests.get(NEWS_RESOURCE_URL, timeout=request_timeout)
+
+        ## Save to file the log for dissection
+        with open("news.log", "w") as file:
+            file.writelines(result.text)
+
+        if result.status_code == 200:
+            return result.text
+
+    except Exception as e:
+        print(f'Unable to gather the company news: {e.args}')
+
+    return result
+
+
+
+def gather_reports(url):
     '''
     Gathers the reports using the provided links of the buy back
     :param report_url: URL of the report from the trading platform
     :return:
     '''
+    try:
 
+        result = requests.get(NEWS_RESOURCE_URL, timeout=request_timeout)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    except Exception as e:
+        print(f'Unable to gather the report: {e.args}')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    print(get_company_news(NEWS_RESOURCE_URL))
