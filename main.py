5ea85cc266279ef668837e620d6f80655de367b9
Jenver <denialsys@gmail.com>
2022-12-06 00:59:27 +0800

    "Added the news resource url, using FMS platform"

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+
+target_company_code = 'MEG'
+NEWS_RESOURCE_URL = 'https://firstmetro.cdn.technistock.net/news/n-psendc-hist-json.asp?code='
+
+def gather_reports(report_url):
+    '''
+    Gathers the reports using the provided links of the buy back
+    :param report_url: URL of the report from the trading platform
+    :return:
+    '''
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
